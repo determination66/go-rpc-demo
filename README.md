509943d5f31eff4d39542fdac8055376357d9d52
dongchuliang <2496417370@qq.com>
2024-03-13 20:18:54 +0800

    feat:替换传输协议为http

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## go语言rpc demo
 
 - Version1:feat:重新生成，go 快速体验rpc开发
+- Version2:feat:替换rpc的序列化协议为json
 - 
--- main/server.go
+++ main/server.go
@@ -3,7 +3,8 @@ package main
 import (
 	"fmt"
 	"go_rpc_demo/global"
-	"net"
+	"io"
+	"net/http"
 	"net/rpc"
 	"net/rpc/jsonrpc"
 )
@@ -24,17 +25,20 @@ func main() {
 		panic("rpc.RegisterName:" + err.Error())
 	}
 
-	listener, err := net.Listen("tcp", ":6666")
-	if err != nil {
-		panic("net.Listen:" + err.Error())
-	}
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			panic("listener.Accept():" + err.Error())
+	http.HandleFunc("/jsonrpc", func(w http.ResponseWriter, r *http.Request) {
+		var conn io.ReadWriteCloser = struct {
+			io.Writer
+			io.ReadCloser
+		}{
+			ReadCloser: r.Body,
+			Writer:     w,
 		}
-		go jsonrpc.ServeConn(conn)
-	}
+		err := rpc.ServeRequest(jsonrpc.NewServerCodec(conn))
+		if err != nil {
+			panic("rpc.ServeRequest" + err.Error())
+		}
+	})
+
+	_ = http.ListenAndServe(":6666", nil)
 
 }
