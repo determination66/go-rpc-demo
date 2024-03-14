b38ab02fb56fdaa677b3a1dec8c9a8c9b5564c9e
dongchuliang <2496417370@qq.com>
2024-03-14 15:59:08 +0800

    feat:生成proto

---
+++ main/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"go_rpc_demo/protoc/hello_world"
+	"google.golang.org/protobuf/proto"
+)
+
+func main() {
+	req := hello_world.HelloRequest{
+		Name:    "Bobby",
+		Age:     18,
+		Courses: []string{"go", "gin", "微服务"},
+	}
+	// proto 解析
+	protoRsp, _ := proto.Marshal(&req)
+	fmt.Println(len(protoRsp))
+
+	// json解析
+	jsonRsp, _ := json.Marshal(&req)
+	fmt.Println(len(jsonRsp))
+
+	//Unmarshal proto的二进制内容
+	newReq := hello_world.HelloRequest{}
+	_ = proto.Unmarshal(protoRsp, &newReq)
+	fmt.Println(newReq.Courses)
+
+}
